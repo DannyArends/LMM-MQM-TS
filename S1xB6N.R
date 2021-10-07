db46af7eaea348a903bfb3081557643bfbbdf48d
DannyArends <Danny.Arends@Gmail.com>
2021-10-07 16:58:12 +0200

    Example analysis of Manuel his data set

---
+++ S1xB6N.R
@@ -0,0 +1,15 @@
+
+setwd("D:/LMMMQMts/")
+source("LMMMQMts.R")
+
+setwd("D:/LMMMQMts/S1xB6N")
+
+genotypes <- read.table("genotypes.txt", sep = "\t", check.names=FALSE, colClasses="character")
+phenotypes <- read.table("phenotypes.txt", sep = "\t")
+covariates <- read.table("covariates.txt", sep = "\t")
+map <- read.table("map.txt", sep = "\t")
+
+covariates <- covariates[,-c(1,3)]
+
+results <- LMMMQMts(genotypes, phenotypes, covariates, map, markers = c("gUNC5046545", "gUNC10595065"), mar.code = c("-1", "0", "1"))
+
